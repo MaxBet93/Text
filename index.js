3e88633a550e1ae0f2b9f6daa83a933c868fb89f
MaxBet93 <114855423+MaxBet93@users.noreply.github.com>
2022-12-15 14:12:19 +0200

    wELL

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function ab(){
+let a = document.getElementById("in").value;
+console.log("Текст " + a);
+}
+
+function dd(){
+  let b =  document.getElementById("num").value;
+  let c =  document.getElementById("num1").value;
+  let d =  document.getElementById("num2").value;
+  let e =  document.getElementById("num3").value;
+  let f =  document.getElementById("num4").value;
+  console.log("Число " + b + " " + c + " " + d + " " + e + " " + f);
+}
+
